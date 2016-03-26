650eb82ff492403d72d179b5c03d10108e1c3d1c
ArtiomCX75 <faa1192@gmail.com>
2016-03-26 17:02:45 +0300

    1 bil 133 sec

--- src/CalcNumbers.java
+++ src/CalcNumbers.java
@@ -1,21 +1,24 @@
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
 
 public class CalcNumbers {
 	public static ArrayList<Integer> list = new ArrayList<>();
 
 	public static void main(String[] args) throws InterruptedException {
 		int time = (int) new Date().getTime();
-		int x1 = 50_000_000;
-		int x2 = 75_000_000;
-		int x3 = 100_000_000;
-		int x4 = 125_000_000;
-		int x5 = 150_000_000;
-		int x6 = 175_000_000;
-		int x7 = 195_000_000;
-		int x8 = 200_000_000;
 
-		CalcThread t0 = new CalcThread(2, x1);
+		int x8 = 1_000_000_000;
+
+		int x1 = x8 / 8;
+		int x2 = x1 * 2;
+		int x3 = x1 * 3;
+		int x4 = x1 * 4;
+		int x5 = x1 * 5;
+		int x6 = x1 * 6;
+		int x7 = x1 * 7;
+
+		CalcThread t0 = new CalcThread(3, x1);
 		CalcThread t1 = new CalcThread(x1, x2);
 		CalcThread t2 = new CalcThread(x2, x3);
 		CalcThread t3 = new CalcThread(x3, x4);
@@ -23,40 +26,40 @@ public class CalcNumbers {
 		CalcThread t5 = new CalcThread(x5, x6);
 		CalcThread t6 = new CalcThread(x6, x7);
 		CalcThread t7 = new CalcThread(x7, x8);
-
+		list.add(2);
 		t0.start();
 
-		while (t0.iter < x1 / 2) {
-			Thread.sleep(1000);
+		while (t0.iter < Math.sqrt(x1)) {
+			Thread.sleep(1);
 		}
 		t1.start();
 
-		while (t0.iter < x2 / 2) {
-			Thread.sleep(1000);
+		while (t0.iter < Math.sqrt(x2)) {
+			Thread.sleep(1);
 		}
 		t2.start();
 
-		while (t1.iter < x3 / 2) {
-			Thread.sleep(1000);
+		while (t0.iter < Math.sqrt(x3)) {
+			Thread.sleep(1);
 		}
 		t3.start();
 
-		while (t1.iter < x4 / 2) {
-			Thread.sleep(1000);
+		while (t0.iter < Math.sqrt(x4)) {
+			Thread.sleep(1);
 		}
 		t4.start();
 
-		while (t1.iter < x5 / 2) {
-			Thread.sleep(1000);
+		while (t0.iter < Math.sqrt(x5)) {
+			Thread.sleep(10);
 		}
 		t5.start();
 
-		while (t2.iter < x6 / 2) {
-			Thread.sleep(1000);
+		while (t0.iter < Math.sqrt(x6)) {
+			Thread.sleep(1);
 		}
 		t6.start();
-		while (t3.iter < x7 / 2) {
-			Thread.sleep(1000);
+		while (t0.iter < Math.sqrt(x7)) {
+			Thread.sleep(1);
 		}
 		t7.start();
 
@@ -70,9 +73,9 @@ public class CalcNumbers {
 		t7.join();
 
 		time = (int) new Date().getTime() - time;
-		System.out.println("count " + list.size());
-		System.out.println("time " + ((double) time / 1000));
 
+		System.out.println("count1 " + list.size());
+		System.out.println("time " + ((double) time / 1000));
 	}
 
 	synchronized public static void addToList(int num) {
--- src/CalcThread.java
+++ src/CalcThread.java
@@ -6,35 +6,45 @@ public class CalcThread extends Thread {
 	public int iter;
 
 	public CalcThread(int from, int to) {
+		if(from%2==0)
+			from--;
+		if(to%2==0)
+			to--;
 		fromnum = from;
 		tonum = to;
 	}
 
 	@Override
 	public void run() {
-		 System.out.println("Thread starts "+this.getName());
+		 System.out.println(this.getName()+" starts");
 		calc();
-		 System.out.println("Thread ends "+this.getName());
+		 System.out.println(this.getName()+" ends");
 	}
 
 	public void calc() {
-		for (iter = fromnum; iter < tonum; iter++) {
+		for (iter = fromnum; iter < tonum; iter+=2) {
+			
 			if (hasDiv(iter)) {
-				// System.out.println("contains " + i);
+			//	 System.out.println(this.getName()+" contains " + iter);
 			} else {
 				CalcNumbers.addToList(iter);
-				// System.out.println("add " + iter);
+			//	 System.out.println(this.getName()+"           add " + iter);
 			}
+		//	if((iter-1)%10_000_000==0)
+		//		System.out.println("Current size is "+CalcNumbers.list.size());
+			
 		}
 	}
 
 	public boolean hasDiv(int num) {
+		
 		for (int i = 0; i < CalcNumbers.list.size(); i++) {
 			if (num % CalcNumbers.list.get(i) == 0) {
 				return true;
 			}
-			if(Math.sqrt(num)<CalcNumbers.list.get(i))
-				break;
+			if(CalcNumbers.list.get(i)>Math.sqrt(num)+1){
+				return false;
+			}
 		}
 		return false;
 	}
